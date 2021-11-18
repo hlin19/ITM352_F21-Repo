5dead92dc05eba5a2a15e0ff9952776e305f4c03
hlin19 <89569993+hlin19@users.noreply.github.com>
2021-11-17 19:46:58 -1000

    asdf

--- Lin_Helen_Assignement_1/server.js
+++ Lin_Helen_Assignement_1/server.js
@@ -10,7 +10,7 @@ var products = require("./product.json"); //load in the products created in Prod
 app.use(myParser.urlencoded({ extended: true }));
 
 // To send data to the client side (code from lab 13 exercise 5)
-app.get("/product_data.js", function (req, res, next) {
+app.get("/products.js", function (req, res, next) {
   res.type(`.js`);
   var products_str = `var products = ${JSON.stringify(products)};`;
   res.send(products_str);
@@ -22,8 +22,26 @@ app.all("", function (request, response, next) {
   next();
 });
 
-// process purchase request (validate quantities, check quantity available)
-// < your code here >
+// Process purchase request (From Lab13 Exercise 4)
+app.post("/process_form", function (request, response) {
+  let POST = request.body;
+  let brand = products[0]["name"];
+  let name_price = products[0]["price"];
+
+  if (typeof POST["quantity_textbox"] != "undefined") {
+    let quantity = POST["quantity_textbox"];
+    if (isNonNegativeInteger(quantity)) {
+      products[0]["total_sold"] += Number(quantity);
+      response.send(
+        `<H2>Thank you for ordering ${quantity} ${brand}! Your total is \$${
+          quantity * name_price
+        }.</H2>`
+      );
+    } else {
+      response.send(`<I>${quantity} is not a valid quantity!</I>`);
+    }
+  }
+});
 
 // route all other GET requests to files in public
 app.use(express.static("./public"));
