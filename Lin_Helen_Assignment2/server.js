cf393dc6a16464b9861cdaf3b33ec81874fb44cb
hlin19 <89569993+hlin19@users.noreply.github.com>
2021-12-14 23:15:32 -1000

    update server.js

--- Lin_Helen_Assignment2/server.js
+++ Lin_Helen_Assignment2/server.js
@@ -72,10 +72,11 @@ app.post("/login", function (request, response) {
 
 app.get("/register", function (request, response) {
   // Give a simple register form
+  // register form found on w3school template
   str = `
  
   <html>
-  <title>W3.CSS</title>
+  <title>Register Account</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -87,7 +88,7 @@ app.get("/register", function (request, response) {
   <div class="w3-row w3-section">
     <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
       <div class="w3-rest">
-        <input class="w3-input w3-border" name="Username" type="text" placeholder="Username">
+        <input class="w3-input w3-border" name="username" type="text" placeholder="Username">
       </div>
   </div>
   
@@ -101,7 +102,7 @@ app.get("/register", function (request, response) {
   <div class="w3-row w3-section">
     <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-pencil"></i></div>
       <div class="w3-rest">
-        <input class="w3-input w3-border" name="Enter Password" type="text" placeholder="Enter Password">
+        <input class="w3-input w3-border" name="password" type="text" placeholder="Enter Password">
       </div>
   </div>
   
