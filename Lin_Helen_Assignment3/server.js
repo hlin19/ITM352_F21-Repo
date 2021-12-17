c9ecd1f4a1ec755ea191e986218679267117dce3
hlin19 <89569993+hlin19@users.noreply.github.com>
2021-12-17 00:58:45 -1000

    css edits

--- Lin_Helen_Assignment3/server.js
+++ Lin_Helen_Assignment3/server.js
@@ -4,6 +4,8 @@ var app = express();
 var fs = require("fs");
 var port = 8080;
 var myParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
 var filename = "./user_data.json";
 var queryString = require("query-string");
 
@@ -11,6 +13,10 @@ var products = require("./product.json"); //load in the products created in Prod
 var quantityArray = ["0", "0", "0", "0", "0"]; //initialize array of quantity
 var currentUser = "";
 
+app.use(
+  session({ secret: "MySecretKey", resave: true, saveUninitialized: true })
+);
+app.use(cookieParser());
 app.use(myParser.urlencoded({ extended: true }));
 
 if (fs.existsSync(filename)) {
@@ -32,19 +38,19 @@ if (fs.existsSync(filename)) {
 // user_data[username].password = "newpass";
 // user_data[username].email = "newuser@user.com";
 
-app.get("/login", function (request, response) {
-  // Give a simple login form
-  str = `
-<body>
-<form action="" method="POST">
-<input type="text" name="username" size="40" placeholder="enter username" ><br />
-<input type="password" name="password" size="40" placeholder="enter password"><br />
-<input type="submit" value="Submit" id="submit">
-</form>
-</body>
-    `;
-  response.send(str);
-});
+// app.get("/", function (request, response) {
+//   // Give a simple login form
+//   str = `
+// <body>
+// <form action="" method="POST">
+// <input type="text" name="username" size="40" placeholder="enter username" ><br />
+// <input type="password" name="password" size="40" placeholder="enter password"><br />
+// <input type="submit" value="Submit" id="submit">
+// </form>
+// </body>
+//     `;
+//   response.send(str);
+// });
 
 app.post("/login", function (request, response) {
   // Process login form POST and redirect to logged in page if ok, back to login page if not
@@ -59,29 +65,68 @@ app.post("/login", function (request, response) {
     if (user_data[user_name].password == user_pass) {
       //redirect to home page if getting a good login
       currentUser = user_name; //store user_name to currentUser and pass to product.js
+      response.cookie("username", currentUser);
       response.redirect("/order_page.html");
     } else {
       // Bad login, redirect
-      response.redirect("/login");
+      response.redirect("/");
     }
   } else {
     // Bad username
-    response.redirect("/login");
+    response.redirect("/");
   }
 });
 
 app.get("/register", function (request, response) {
   // Give a simple register form
+  // register form found on w3school template
   str = `
-<body>
-<form action="/register" method="POST">
-<input type="text" name="username" size="40" placeholder="enter username" ><br />
-<input type="password" name="password" size="40" placeholder="enter password"><br />
-<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
-<input type="email" name="email" size="40" placeholder="enter email"><br />
-<input type="submit" value="Submit" id="submit">
-</form>
-</body>
+ 
+  <html>
+  <title>Register Account</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <body>
+  
+  <form action="/register" method="POST">
+  <h2 class="w3-center">Sign Up Now!</h2>
+   
+  <div class="w3-row w3-section">
+    <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
+      <div class="w3-rest">
+        <input class="w3-input w3-border" name="username" type="text" placeholder="Username">
+      </div>
+  </div>
+  
+  <div class="w3-row w3-section">
+    <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-envelope-o"></i></div>
+      <div class="w3-rest">
+        <input class="w3-input w3-border" name="email" type="text" placeholder="Email">
+      </div>
+  </div>
+  
+  <div class="w3-row w3-section">
+    <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-pencil"></i></div>
+      <div class="w3-rest">
+        <input class="w3-input w3-border" name="password" type="text" placeholder="Enter Password">
+      </div>
+  </div>
+  
+  <div class="w3-row w3-section">
+    <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-pencil"></i></div>
+      <div class="w3-rest">
+        <input class="w3-input w3-border" name="Enter Password Again" type="text" placeholder="Enter Password Again">
+      </div>
+  </div>
+  
+  <p class="w3-center">
+  <button class="w3-button w3-section w3-blue w3-ripple"> Register </button>
+  </p>
+  </form>
+  
+  </body>
+  </html> 
     `;
   response.send(str);
 });
@@ -130,9 +175,8 @@ app.post("/register", function (request, response) {
 // To send product data to the client side (code from lab 13 exercise 5)
 app.get("/products.js", function (req, res, next) {
   res.type(`.js`);
-  var str = `var currentUser = "${currentUser}"; var products = ${JSON.stringify(
-    products
-  )};`;
+  var str = `var products = ${JSON.stringify(products)};`;
+
   res.send(str);
 });
 
@@ -146,7 +190,7 @@ app.all("", function (request, response, next) {
 // Process purchase request (From Lab13 Exercise 4)
 app.post("/process_form", function (request, response) {
   let POST = request.body;
-
+  request.session.order_array = [];
   for (i = 0; i < products.Milk_Tea.length; i++) {
     let quantity = POST[`quantity${i}`];
     if (quantity == undefined) {
@@ -159,11 +203,16 @@ app.post("/process_form", function (request, response) {
       quantity = "0";
     } else {
       quantityArray[i] = quantity;
-      response.send(`<p>Item added to cart successfully!</p>`);
+
+      for (i = 0; i < products.Milk_Tea.length; i++) {
+        request.session.order_array[i] = quantityArray[i];
+      }
+
+      response.redirect("/cart.html");
     }
   }
 
-  console.log(quantityArray);
+  console.log(request.session["order_array"]);
 });
 
 //Data validation (found in Lab 3 Exercise 5)
@@ -183,10 +232,13 @@ function isNonNegativeInteger(inputString, returnErrors = false) {
 }
 
 //to send invoice data to client side (From Lab 13 Exercise 4)
-app.get("/invoice.js", function (req, res, next) {
+app.get("/cart.js", function (req, res, next) {
   res.type(`.js`);
-  var products_str = `var quantityArray = ${JSON.stringify(quantityArray)};
-                      var products = ${JSON.stringify(products)};`;
+  var orderArray = req.session.order_array;
+  var username = req.cookies["username"];
+  var products_str = `var quantityArray = ${JSON.stringify(orderArray)};
+                      var products = ${JSON.stringify(products)};
+                      var currentUser = "${username}"; `;
   res.send(products_str);
 });
 
