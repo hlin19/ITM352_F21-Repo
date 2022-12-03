164f778b2eb553dbb7e7050d75959ce2679c88c4
E-Melchor <emi96765@hawaii.edu>
2022-12-02 21:43:09 -1000

    Monthly_spending
    Added monthly_spending page, connected to drink on database

--- Lin_Helen_Assignment3/server.js
+++ Lin_Helen_Assignment3/server.js
@@ -16,7 +16,7 @@ var con = mysql.createConnection({
     host: '127.0.0.1',
     user: "root",
     port: 3306,
-    database: "Travel",
+    database: "RRT",
     password: ""
 });
 
@@ -267,7 +267,7 @@ function query_DB(POST, response) {
         isNonNegativeInteger(POST['high_price'])) { // Only query if we got a low and high price
         low = POST['low_price']; // Grab the parameters from the submitted form
         high = POST['high_price'];
-        query = "SELECT * FROM Room where price > " + low + " and price < " + high; // Build the query string
+        query = "SELECT * FROM Drink where price > " + low + " and price < " + high; // Build the query string
         con.query(query, function(err, result, fields) { // Run the query
             if (err) throw err;
             console.log(result);
@@ -276,21 +276,19 @@ function query_DB(POST, response) {
             console.log(res_json);
 
             // Now build the response: table of results and form to do another query
-            response_form = `<form action="Room-query.html" method="GET">`;
+            response_form = `<form action="monthly_spending.html" method="GET">`;
             response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
-            response_form += `<td><B>Room#</td><td><B>Hotel#</td><td><B>Type</td><td><B>Price</td></b>`;
+            response_form += `<td><B>Drink Name</td><td><B>Price</td></b>`;
             for (i in res_json) {
-                response_form += `<tr><td> ${res_json[i].roomNo}</td>`;
-                response_form += `<td> ${res_json[i].hotelNo}</td>`;
-                response_form += `<td> ${res_json[i].type}</td>`;
-                response_form += `<td> ${res_json[i].price}</td></tr>`;
+                response_form += `<tr><td> ${res_json[i].DrinkName}</td>`;
+                response_form += `<td> $${res_json[i].Price}</td></tr>`;
             }
             response_form += "</table>";
-            response_form += `<input type="submit" value="Another Query?"> </form>`;
+            response_form += `<input type="submit" value="Click to Go Back"> </form>`;
             response.send(response_form);
         });
     } else {
-        response.send("Enter some prices doofus!");
+        response.send("Please go back and enter a price range. Thank you! :)");
     }
 }
 
