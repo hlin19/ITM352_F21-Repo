a47c58d8db167654bcb0d5cb49117113341162d1
E-Melchor <emi96765@hawaii.edu>
2022-12-04 17:15:51 -1000

    start set up of rewards page form

--- Lin_Helen_Assignment3/server.js
+++ Lin_Helen_Assignment3/server.js
@@ -261,7 +261,7 @@ app.get("/cart.js", function(req, res, next) {
 // route all other GET requests to files in public (Assignment 1 Instruction example)
 app.use(express.static("./public"));
 
-//add for RRT
+//add for RRT Monthly Spending
 function query_DB(POST, response) {
     if (isNonNegativeInteger(POST['low_price']) &&
         isNonNegativeInteger(POST['high_price'])) { // Only query if we got a low and high price
@@ -292,8 +292,45 @@ function query_DB(POST, response) {
     }
 }
 
-//add for RRT
+//add for RRT Monthly Spending
 app.post("/process_query", function(request, response) {
+    let POST = request.body;
+    query_DB(POST, response);
+});
+
+//add for RRT Customer Rewards page
+function query_DB(POST, response) {
+    if (isNonNegativeInteger(POST['low_price']) &&
+        isNonNegativeInteger(POST['high_price'])) { // Only query if we got a low and high price
+        low = POST['low_price']; // Grab the parameters from the submitted form
+        high = POST['high_price'];
+        query = "SELECT * FROM Drink where price > " + low + " and price < " + high; // Build the query string
+        con.query(query, function(err, result, fields) { // Run the query
+            if (err) throw err;
+            console.log(result);
+            var res_string = JSON.stringify(result);
+            var res_json = JSON.parse(res_string);
+            console.log(res_json);
+
+            // Now build the response: table of results and form to do another query
+            response_form = `<form action="rewards_page.html" method="GET">`;
+            response_form += `<table border="3" cellpadding="5" cellspacing="5">`;
+            response_form += `<td><B>Drink Name</td><td><B>Price</td></b>`;
+            for (i in res_json) {
+                response_form += `<tr><td> ${res_json[i].DrinkName}</td>`;
+                response_form += `<td> $${res_json[i].Price}</td></tr>`;
+            }
+            response_form += "</table>";
+            response_form += `<input type="submit" value="Click to Go Back"> </form>`;
+            response.send(response_form);
+        });
+    } else {
+        response.send("Please go back and login. Thank you! :)");
+    }
+}
+
+//add for RRT Customer Reward
+app.post("/process_customer_reward", function(request, response) {
     let POST = request.body;
     query_DB(POST, response);
 });
