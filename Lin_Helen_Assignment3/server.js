50956a743bafea9d79842f5c305b1089a2ab0296
E-Melchor <emi96765@hawaii.edu>
2022-12-06 00:26:57 -1000

    change port to 8080

--- Lin_Helen_Assignment3/server.js
+++ Lin_Helen_Assignment3/server.js
@@ -14,7 +14,7 @@ console.log("Connecting to localhost...");
 var con = mysql.createConnection({
     host: '127.0.0.1',
     user: "root",
-    port: 3306,
+    port: 8080,
     database: "RRT",
     password: ""
 });
